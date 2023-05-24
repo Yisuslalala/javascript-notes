f1250b5e6f9a54d28ae87d6f8e15b0cd300304e8
Jesús Quiñones <1930318@upv.edu.mx>
2023-05-23 23:54:18 -0600

    first topics that I had troubles

---
+++ UpdatingProperties.js
@@ -0,0 +1,21 @@
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  // const latestName = myDog["name"];
+  // console.log(latestName);
+  const newName = "Happy Coder";
+  console.log(newName);
+  myDog["name"] = newName;
+  
+  /**
+   * Also you can do:
+   *  myDog["name"] = "Happy Coder";
+   *  or:
+   *  myDog.name = "Happy Coder";
+   */
